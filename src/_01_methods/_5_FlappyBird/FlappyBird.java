228d3ca69fcbc671954c810a56808bd5cac64997
league <maddoxseul@sbsd.net>
2023-06-17 16:29:40 -0700

    brid

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -5,7 +5,14 @@ import processing.core.PApplet;
 public class FlappyBird extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-
+    int x=400;
+    int y=300;
+    int birdYVelocity = 1;
+    int gravity = 1;
+    int X=600;
+    int pipeGap=200;
+    int upperPipeHeight = (int) random(100, 400);
+    int lowerY = upperPipeHeight +pipeGap;
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -18,12 +25,51 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void draw() {
-    	background(0,0,100);
+    	if(intersectsPipes()) {
+    		exit();
+    	}
+    	birdYVelocity=birdYVelocity+gravity;
+    	y=y+birdYVelocity;
+    	background(0,0,150);
     	fill(100,0,0);
     	stroke(100,0,0);
-    	ellipse(400,300,,);
+    	ellipse(x,y,20,20);
+    	fill(0,200,0);
+    	X-=2;
+    	rect(X,-1,50,upperPipeHeight);
+    	rect(X,lowerY,50,400);
+    	rect();
+    	
+    	
+    	if(X<=-50) {
+    	upperPipeHeight = (int) random(100, 400);
+    	lowerY = upperPipeHeight +pipeGap;
+    		X+=850;
+    	}
     }
-
+    boolean intersectsPipes() { 
+        if (y < upperPipeHeight && x > X && x < (X+50)){
+           return true; }
+       else if (y>lowerY && x > X && x < (X+50)) {
+           return true; }
+       else { return false; }
+}
+    
+    public void mousePressed(){
+    	if(mousePressed) {
+    		birdYVelocity=-15;
+    	}
+    }
+    public void teleportPipes(){
+    	if(X<=-50) {
+    		X+=650;
+    		rect(X,-1,50,400);
+    	}
+    }
+    
+    
+    
+    
     static public void main(String[] args) {
         PApplet.main(FlappyBird.class.getName());
     }
